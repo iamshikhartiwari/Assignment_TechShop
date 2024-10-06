66392717cef88364553ea618a600aac740387403
Shikhar <iamshikhartiwari@gmail.com>
2024-10-06 19:19:03 +0530

    adding c# assisment

---
+++ C#/ElectronicGadgets.Entity/ElectronicGadgets.UI/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using ElectronicGadgets.BussnissLayerLogic;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        int result = 5 +5 +5 +5;
+        Console.WriteLine($"The result is: {result}");
+    }
+}
