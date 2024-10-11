66931a2f13369712a092847a1f00174b6447d739
Shikhar Tiwari <72856503+iamshikhartiwari@users.noreply.github.com>
2024-10-11 16:16:10 +0530

    Update OrderDetail.cs

--- C#/ElectronicGadgets.Entity/ElectronicGadgets.Entity/OrderDetail.cs
+++ C#/ElectronicGadgets.Entity/ElectronicGadgets.Entity/OrderDetail.cs
@@ -9,26 +9,5 @@ public class OrderDetail
     public Products Product { get; set; }
     public int Quantity { get; set; }
 
-    public decimal CalculateSubtotal()
-    {
-        return Quantity * Product.Price;
-    }
-
-    public void GetOrderDetailInfo()
-    {
-        Console.WriteLine($"Order Detail ID: {OrderDetailID}");
-        Console.WriteLine($"Product: {Product.ProductName}");
-        Console.WriteLine($"Quantity: {Quantity}");
-        Console.WriteLine($"Subtotal: {CalculateSubtotal():C}"); 
-    }
-
-    public void UpdateQuantity(int newQuantity)
-    {
-        Quantity = newQuantity;
-    }
-
-    public void AddDiscount(decimal discountPercentage)
-    {
-        // Logic to apply a discount (not implemented here)
-    }
+  
 }
