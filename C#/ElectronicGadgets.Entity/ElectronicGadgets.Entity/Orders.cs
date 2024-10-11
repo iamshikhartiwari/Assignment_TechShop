ef10cb1f3461b6a6a3f3ee7bb744a3fca8595a56
Shikhar Tiwari <72856503+iamshikhartiwari@users.noreply.github.com>
2024-10-11 16:16:26 +0530

    Update Orders.cs

--- C#/ElectronicGadgets.Entity/ElectronicGadgets.Entity/Orders.cs
+++ C#/ElectronicGadgets.Entity/ElectronicGadgets.Entity/Orders.cs
@@ -11,27 +11,6 @@ namespace ElectronicGadgets.Models;
         public DateTime OrderDate { get; set; }
         public decimal TotalAmount { get; set; }
 
-        public void CalculateTotalAmount()
-        {
-            TotalAmount = 0; 
-        }
-
-        public void GetOrderDetails()
-        {
-            Console.WriteLine($"Order ID: {OrderID}");
-            Console.WriteLine($"Customer: {Customer.FirstName} {Customer.LastName}");
-            Console.WriteLine($"Order Date: {OrderDate}");
-            Console.WriteLine($"Total Amount: {TotalAmount:C}"); // Format as currency
-        }
-
-        public void UpdateOrderStatus(string status)
-        {
-            // Update order status logic
-        }
-
-        public void CancelOrder()
-        {
-            // Logic to cancel the order
-        }
+       
     }
 
